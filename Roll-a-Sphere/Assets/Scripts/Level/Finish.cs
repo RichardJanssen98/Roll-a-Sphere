64ec6506c121f135c9b1e10ff2f84a0ae8912503
Richard Janssen <richardjanssen98@gmail.com>
2020-04-07 12:01:23 +0200

    Added finish line and updated prefabs

---
+++ Roll-a-Sphere/Assets/Scripts/Level/Finish.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField]
+    LevelManager levelManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            levelManager.EndGame();
+        }
+    }
+}
---
+++ Roll-a-Sphere/Assets/Scripts/Level/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("TO-DO: Add API call to Scoreboard");
+        Debug.Log("TO-DO: Change scene to menu");
+    }
+}
