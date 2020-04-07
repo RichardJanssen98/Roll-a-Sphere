fdbd59e1d2cce0e5c617b56252339d6dbae48f86
Richard Janssen <richardjanssen98@gmail.com>
2020-04-07 11:11:29 +0200

    Added player movement controls

---
+++ Roll-a-Sphere/Assets/Scripts/PickUps/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField]
+    private int scoreValue = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spin around
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            other.GetComponent<PlayerMovement>().CallIncreaseScore(scoreValue);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Roll-a-Sphere/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    [SerializeField]
+    private ScoreManager scoreManager;
+
+    private float speed = 1;
+    private float jumpingForce = 10f;
+
+    private bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(gameObject.transform.position, new Vector3(0, -1, 0), 1f))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        //Left (A)
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rigidbody.AddForce(new Vector3(-speed, 0, 0));
+        }
+        //Right (D)
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rigidbody.AddForce(new Vector3(speed, 0, 0));
+        }
+        //Backward (S)
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            rigidbody.AddForce(new Vector3(0, 0, -speed));
+        }
+        //Forward (W)
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rigidbody.AddForce(new Vector3(0, 0, speed));
+        }
+
+        //Jump (Space)
+        if (Input.GetAxis("Jump") > 0 && isGrounded)
+        {
+            rigidbody.AddForce(new Vector3(0, jumpingForce, 0));
+        }
+    }
+
+    public void CallIncreaseScore(int scoreValue)
+    {
+        scoreManager.IncreaseScore(scoreValue);
+    }
+}
